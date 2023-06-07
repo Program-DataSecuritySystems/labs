b74bbbf60998f9a4d3e98c87c706e6bf697bb456
Viktor Zozuliuk <zozulyuk22@gmail.com>
2023-06-07 23:27:29 +0300

    [lab 6] added decryption

--- Application/DigitalSignature/SymmetricEncryption.cs
+++ Application/DigitalSignature/SymmetricEncryption.cs
@@ -63,4 +63,41 @@ public class SymmetricEncryption
 
         return Convert.ToBase64String(signatureBytes);
     }
+
+    // Метод для дешифрування повідомлення з використанням симетричного шифру
+    public static string Decrypt(string encryptedMessage, string key)
+    {
+        byte[] encryptedBytes = Convert.FromBase64String(encryptedMessage);
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+        byte[] decryptedBytes = new byte[encryptedBytes.Length];
+
+        for (int i = 0; i < encryptedBytes.Length; i += _blockSize)
+        {
+            byte[] encryptedBlock = new byte[_blockSize];
+            Array.Copy(encryptedBytes, i, encryptedBlock, 0, _blockSize);
+
+            // Виконати дешифрування блоку
+            byte[] decryptedBlock = DecryptBlock(encryptedBlock, keyBytes);
+
+            Array.Copy(decryptedBlock, 0, decryptedBytes, i, _blockSize);
+        }
+
+        return Encoding.UTF8.GetString(decryptedBytes).TrimEnd('\0');
+    }
+
+    // Метод для дешифрування одного блоку
+    private static byte[] DecryptBlock(byte[] encryptedBlock, byte[] key)
+    {
+        // TODO: Реалізувати логіку дешифрування блоку
+        // Використовуйте key для дешифрування блоку і повертайте результат
+        // (наприклад, можна використовувати XOR-операцію для простоти, але це не надійно)
+        byte[] decryptedBlock = new byte[encryptedBlock.Length];
+
+        for (int i = 0; i < encryptedBlock.Length; i++)
+        {
+            decryptedBlock[i] = (byte)(encryptedBlock[i] ^ key[i % key.Length]);
+        }
+
+        return decryptedBlock;
+    }
 }
